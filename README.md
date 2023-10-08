4f806052a48bb5b5d97835fabaa57b305eeafc48
Ethan Walker <inclushe@gmail.com>
2023-10-07 23:26:28 -0400

    Init repo

---
+++ README.md
@@ -0,0 +1,60 @@
+# subtitle-renamer
+
+## Ideas
+
+- Two strategies
+  - Episode number in filenames
+    - Anitony
+  - File order
+    - Backup if episode number is not found on either videos or subtitles
+- Map subtitles to videos
+- Gotchas (backburner)
+  - Dragging folders
+    - Only affect top level folder
+    - Would need to be natural sorted
+      - https://www.npmjs.com/package/natural-compare
+    - Chromium only returns max 100 entries
+      - https://developer.mozilla.org/en-US/docs/Web/API/DataTransferItem/webkitGetAsEntry
+  - Subtitles are not in sync
+    - Add way to batch offset subtitles
+
+## Plan
+
+- [ ] Initialize Anitony
+- [x] Drag episodes
+  - Move each to episode context object
+    - Key is entry.name
+    - Push empty array to entry.name
+    - Push context to [entry.name][file]
+    - Start anitony processing and push result to [entry.name][info]
+  - Update table
+- [x] Drag subtitles
+  - If match these file extensions
+    - "srt", "ass", "idx", "sub", "rt", "ssa", "mks", "vtt", "sup", "scc", "smi", "lrc", "pgs", "txt", "utf", "utf8", "utf-8"
+  - Move each to subtitle content object
+    - Key is entry.name
+    - Push empty array to entry.name
+    - Push context to [entry.name][file]
+    - Push data to [entry.name][data]
+    - Start anitony processing and push result to [entry.name][info]
+  - Update table
+- Download
+  - [x] Episode Number method
+    - [x] Check if each episode and subtile info if episode_number exists
+      - Create episodesNumberMap and subtitlesNumberMap, key is episode_number, value is entry.name
+    - [x] Check if there is one episode episode_number to one subtitle episode_number
+      - Use Number() to compare
+      - Create episodesSubtitlesMap, key is episode entry.name, value is subtitle entry.name
+    - If anything fails, fallback to file order method
+  - [x] File Order method
+    - Create episodesSubtitlesMap, key is episode entry.name, value is subtitle entry.name
+      - 1st episode entry.name to 1st subtitle entry.name, etc.
+      - If there are more subtitles than episodes, ignore the rest
+      - If there are more episodes than subtitles, ignore the rest
+  - Create zip folder with JSZip
+    - https://stuk.github.io/jszip/
+  - For each episode in episodesSubtitlesMap
+    - Add file with key name + extension with subtitle data to zip folder
+  - Generate zip folder
+  - Save zip folder
+    - https://github.com/eligrey/FileSaver.js/
---
+++ index.js
@@ -0,0 +1,194 @@
+var JSZip = require("jszip");
+var FileSaver = require('file-saver');
+var episodes = {};
+var subtitles = {};
+const episodesDropZoneElement = document.querySelector("#episodesDropZone");
+const subtitlesDropZoneElement = document.querySelector("#subtitlesDropZone");
+const subtitlesDownloadButtonElement = document.querySelector("#subtitlesDownloadButton");
+const tableBodyElement = document.querySelector("#tableBody");
+
+initializeAnitomy();
+episodesDropZoneElement.addEventListener("drop", handleEpisodesDropZoneDrop);
+episodesDropZoneElement.addEventListener("dragover", dragOverHandler);
+episodesDropZoneElement.addEventListener("dragleave", dragLeaveHandler);
+subtitlesDropZoneElement.addEventListener("drop", handleSubtitlesDropZoneDrop);
+subtitlesDropZoneElement.addEventListener("dragover", dragOverHandler);
+episodesDropZoneElement.addEventListener("dragleave", dragLeaveHandler);
+subtitlesDownloadButtonElement.addEventListener("click", downloadSubtitles);
+
+function initializeAnitomy() {
+  window.anitomyscript('init').then((res) => console.log(res));
+}
+
+function handleEpisodesDropZoneDrop(event) {
+  event.preventDefault();
+  event.target.closest('.drop-zone').classList.remove("drop-zone--over");
+  episodes = {};
+  for (var i = 0; i < event.dataTransfer.items.length; i++) {
+    let entry = event.dataTransfer.items[i].webkitGetAsEntry();
+    if (entry.isFile) {
+      episodes[entry.name] = {};
+      episodes[entry.name]['file'] = event.dataTransfer.files[i];
+      window.anitomyscript(entry.name)
+        .then((res) => {
+          episodes[entry.name]['info'] = res;
+          updateTable();
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+  updateTable();
+}
+
+function handleSubtitlesDropZoneDrop(event) {
+  event.target.closest('.drop-zone').classList.remove("drop-zone--over");
+  event.preventDefault();
+  subtitles = {};
+  for (var i = 0; i < event.dataTransfer.items.length; i++) {
+    let entry = event.dataTransfer.items[i].webkitGetAsEntry();
+    if (entry.isFile) {
+      subtitles[entry.name] = {};
+      subtitles[entry.name]['file'] = event.dataTransfer.files[i];
+      let reader = new FileReader();
+      reader.onload = function (event) {
+        subtitles[entry.name]['data'] = event.target.result;
+        updateTable();
+      };
+      reader.readAsDataURL(event.dataTransfer.files[i]);
+      window.anitomyscript(entry.name)
+        .then((res) => {
+          subtitles[entry.name]['info'] = res;
+          updateTable();
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+  updateTable();
+}
+
+function downloadSubtitles(event) {
+  event.preventDefault();
+  let episodesSubtitlesMap = mapEpisodesToSubtitlesUsingEpisodeNumber();
+  // if not defined
+  if (episodesSubtitlesMap) {
+    console.log('Using episode number');
+  } else {
+    episodesSubtitlesMap = mapEpisodesToSubtitlesUsingFileOrder();
+    console.log('Using file order');
+  }
+  let zipFolder = new JSZip();
+  let folderName = episodes[Object.keys(episodes)[0]]['info']['anime_title'];
+  if (!folderName) {
+    folderName = 'subtitles';
+  }
+  for (let episodeName in episodesSubtitlesMap) {
+    let subtitleName = episodesSubtitlesMap[episodeName];
+    let newSubtitleFileName = episodes[episodeName]['info']['file_name'] + '.' + subtitles[subtitleName]['info']['file_extension'];
+    zipFolder.file(newSubtitleFileName, subtitles[subtitleName]['data'].split('base64,')[1], {base64: true});
+  }
+  zipFolder.generateAsync({type:"blob"})
+    .then(function(content) {
+        FileSaver.saveAs(content, folderName + ".zip");
+    });
+}
+
+function mapEpisodesToSubtitlesUsingEpisodeNumber() {
+  var episodesNumberMap = {};
+  var subtitlesNumberMap = {};
+  var episodesSubtitlesMap = {};
+
+  // Check episode number exists for each episode
+  for (let episode in episodes) {
+    if (episodes[episode]['info'] && episodes[episode]['info']['episode_number'] && !episodesNumberMap[episodes[episode]['info']['episode_number']]) {
+      episodesNumberMap[episodes[episode]['info']['episode_number']] = episode;
+    } else {
+      return;
+    }
+  }
+
+  // Check episode number exists for each subtitle
+  for (let subtitle in subtitles) {
+    if (subtitles[subtitle]['info'] && subtitles[subtitle]['info']['episode_number'] && !subtitlesNumberMap[subtitles[subtitle]['info']['episode_number']]) {
+      subtitlesNumberMap[subtitles[subtitle]['info']['episode_number']] = subtitle;
+    } else {
+      return;
+    }
+  }
+
+  // Bind each episode to subtitle by episode number
+  for (let episodeNumber in episodesNumberMap) {
+    let numberFound = false;
+    for (let subtitleNumber in subtitlesNumberMap) {
+      if (episodeNumber === subtitleNumber || Number(episodeNumber) == Number(subtitleNumber)) {
+        episodesSubtitlesMap[episodesNumberMap[episodeNumber]] = subtitlesNumberMap[subtitleNumber];
+        numberFound = true;
+      }
+    }
+    if (numberFound === false) {
+      return;
+    }
+  }
+
+  return episodesSubtitlesMap;
+}
+
+function mapEpisodesToSubtitlesUsingFileOrder() {
+  var episodesSubtitlesMap = {};
+  const episodesKeys = Object.keys(episodes);
+  const subtitlesKeys = Object.keys(subtitles);
+  for (let i = 0; i < episodesKeys.length; i++) {
+    episodesSubtitlesMap[episodesKeys[i]] = subtitlesKeys[i];
+  }
+  return episodesSubtitlesMap;
+}
+
+function updateTable() {
+  tableBodyElement.innerHTML = "";
+  const episodesKeys = Object.keys(episodes);
+  const subtitlesKeys = Object.keys(subtitles);
+  let subtitlesLoaded = 0;
+  for (let i = 0; i < Math.max(episodesKeys.length, subtitlesKeys.length); i++) {
+    var row = tableBodyElement.insertRow();
+    var episodeCell = row.insertCell(0);
+    var epInfoCell = row.insertCell(1);
+    var subtitleCell = row.insertCell(2);
+    var subInfoCell = row.insertCell(3);
+    var subDataCell = row.insertCell(4);
+    if (episodesKeys[i]) {
+      episodeCell.innerHTML = episodesKeys[i];
+      if (episodes[episodesKeys[i]]['info'] && episodes[episodesKeys[i]]['info']['episode_number']) {
+        epInfoCell.innerHTML = episodes[episodesKeys[i]]['info']['episode_number'];
+      } else {
+        epInfoCell.innerHTML = '-';
+      }
+    }
+    if (subtitlesKeys[i]) {
+      subtitleCell.innerHTML = subtitlesKeys[i];
+      if (subtitles[subtitlesKeys[i]]['info'] && subtitles[subtitlesKeys[i]]['info']['episode_number']) {
+        subInfoCell.innerHTML = subtitles[subtitlesKeys[i]]['info']['episode_number'];
+      } else {
+        subInfoCell.innerHTML = '-';
+      }
+      if (subtitles[subtitlesKeys[i]]['data']) {
+        subDataCell.innerHTML = 'Stored';
+        subtitlesLoaded++;
+      } else {
+        subDataCell.innerHTML = '-';
+      }
+    }
+    tableBodyElement.appendChild(row);
+  }
+  if (subtitlesLoaded === subtitlesKeys.length && subtitlesKeys.length > 0 && episodesKeys.length > 0) {
+    subtitlesDownloadButtonElement.disabled = false;
+  }
+}
+
+function dragOverHandler(event) {
+  event.preventDefault();
+  event.target.closest('.drop-zone').classList.add("drop-zone--over");
+}
+
+function dragLeaveHandler(event) {
+  event.preventDefault();
+  event.target.closest('.drop-zone').classList.remove("drop-zone--over");
+}
